457b3075e8a7922e1165a7774fd6b74d9bc73387
Samiran Saha <samiran.s89@gmail.com>
2025-11-14 02:28:04 +0530

    config file

--- test/examples/run-parallel-test/parallel.conf.js
+++ test/examples/run-parallel-test/parallel.conf.js
@@ -1,61 +1,78 @@
-exports.config = {
-  user: process.env.BROWSERSTACK_USERNAME || 'BROWSERSTACK_USERNAME',
-  key: process.env.BROWSERSTACK_ACCESS_KEY || 'BROWSERSTACK_ACCESS_KEY',
+console.log("CAPS_JSON" + process.env.BSTACK_CAPS_JSON)
 
-  hostname: 'hub.browserstack.com',
+exports.config = {
+  user: process.env.BROWSERSTACK_USERNAME || "BROWSERSTACK_USERNAME",
+  key: process.env.BROWSERSTACK_ACCESS_KEY || "BROWSERSTACK_ACCESS_KEY",
+
+  hostname: "hub.browserstack.com",
 
   services: [
     [
-      'browserstack',
+      "browserstack",
       {
+        testObservability: true,
+        testObservabilityOptions: {
+          buildTag: "run-bstack-now-web",
+          buildIdentifier: "#${BUILD_NUMBER}",
+        },
         percy: true,
         percyCaptureMode: "auto",
         accessibility: true,
-        buildIdentifier: '${BUILD_NUMBER}',
-        browserstackLocal: true,
-        opts: { forcelocal: false, localIdentifier: "webdriverio-appium-app-browserstack-repo" },
-        app: process.env.BROWSERSTACK_APP,
-      }
-    ]
+        accessibilityOptions: {
+          wcagVersion: "wcag21a",
+          includeIssueType: {
+            bestPractice: true,
+            needsReview: true,
+            experimental: false,
+            advanced: false,
+          },
+          buildIdentifier: "${BUILD_NUMBER}",
+          browserstackLocal: true,
+          opts: {
+            forcelocal: false,
+            localIdentifier: "webdriverio-appium-app-browserstack-repo",
+          },
+          app: process.env.BROWSERSTACK_APP,
+        },
+      },
+    ],
   ],
 
-  capabilities: require('./../../../../usage_file.json'),
+  capabilities: JSON.parse(process.env.BSTACK_CAPS_JSON),
 
   commonCapabilities: {
-    'bstack:options': {
+    "bstack:options": {
       projectName: "NOW-Mobile-Test",
-      buildName: 'browserstack-build-mobile',
+      buildName: "browserstack-build-mobile",
       debug: true,
       networkLogs: true,
-      source: 'webdriverio:appium-sample-sdk:v1.0'
-    }
+      source: "webdriverio:appium-sample-sdk:v1.0",
+    },
   },
 
   maxInstances: parseInt(process.env.BSTACK_PARALLELS) || 10,
 
   updateJob: false,
-  specs: [
-    './specs/single_test.js'
-  ],
+  specs: ["./specs/single_test.js"],
   exclude: [],
 
-  logLevel: 'info',
+  logLevel: "info",
   coloredLogs: true,
-  screenshotPath: './errorShots/',
-  baseUrl: '',
+  screenshotPath: "./errorShots/",
+  baseUrl: "",
   waitforTimeout: 10000,
   connectionRetryTimeout: 90000,
   connectionRetryCount: 3,
 
-  framework: 'mocha',
+  framework: "mocha",
   mochaOpts: {
-    ui: 'bdd',
-    timeout: 240000
-  }
+    ui: "bdd",
+    timeout: 240000,
+  },
 };
 
 // Code to support common capabilities
-exports.config.capabilities.forEach(function(caps){
-  for(let key in exports.config.commonCapabilities) 
-    caps[key] = { ...caps[key], ...exports.config.commonCapabilities[key]};
+exports.config.capabilities.forEach(function (caps) {
+  for (let key in exports.config.commonCapabilities)
+    caps[key] = { ...caps[key], ...exports.config.commonCapabilities[key] };
 });
