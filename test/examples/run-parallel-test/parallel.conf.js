ba217b3ee0448986cb1769999460169389fa8870
Samiran Saha <samiran.s89@gmail.com>
2025-11-14 02:33:44 +0530

    log line remove

--- test/examples/run-parallel-test/parallel.conf.js
+++ test/examples/run-parallel-test/parallel.conf.js
@@ -1,5 +1,3 @@
-console.log("CAPS_JSON" + process.env.BSTACK_CAPS_JSON)
-
 exports.config = {
   user: process.env.BROWSERSTACK_USERNAME || "BROWSERSTACK_USERNAME",
   key: process.env.BROWSERSTACK_ACCESS_KEY || "BROWSERSTACK_ACCESS_KEY",
